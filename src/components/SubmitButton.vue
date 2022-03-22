4a6a1fcc7ba639773af52bb82c78c77ffe4e5de5
Hector Vurchio <hectorvurchio@gmail.com>
2022-03-22 13:02:07 -0400

    append FormService.js

--- src/components/SubmitButton.vue
+++ src/components/SubmitButton.vue
@@ -7,59 +7,26 @@
 </template>
 <script>
 import DemoService from "@/services/DemoService.js";
-
+import FormService from "@/services/FormService.js";
 export default {
   name: "SubmitButton",
   data() {
     return {
       files: [],
       fields: {},
-      fieldBC: "#deeaeb",
       aaids: [],
+      play: true,
     };
   },
   methods: {
     submitFiles(event) {
+      const fs = FormService.chkEmptyElm(event.target);
+      this.files = fs[0];
+      this.fields = fs[1];
+      this.aaids = fs[2];
+      this.play = fs[3];
       const form = event.target.form;
-      const elements = form.elements;
-      let play = true;
-      /*console.log(elements);*/
-      for (let i = 0; i < elements.length; i++) {
-        if (
-          elements[i].nodeName.toLowerCase() == "input" &&
-          elements[i].type.toLowerCase() == "file"
-        ) {
-          //loading of input files
-          const aaid = `${elements[i].id}-p`;
-          if (elements[i].files[0] != undefined) {
-            this.files.push(elements[i].files[0]);
-            this.aaids.push(aaid);
-          } else {
-            const labelid = `${elements[i].id}-l`;
-            const label = document.getElementById(`${labelid}`);
-            const aa = document.getElementById(`${aaid}`);
-            alert(`Please Select ${label.textContent} File`);
-            label.style.color = "red";
-            aa.style.color = "red";
-            play = false;
-            break;
-          }
-        } else if (elements[i].nodeName.toLowerCase() != "textarea") {
-          //loading of input and select fields
-          if (elements[i].value != "") {
-            this.fields[`${elements[i].id}`] = elements[i].value;
-            elements[i].style.borderColor = "#deeaeb";
-          } else {
-            elements[i].style.borderColor = "red";
-            alert("Please Fill In the Required Fields in Red");
-            play = false;
-            break;
-          }
-        } else {
-          this.fields[`${elements[i].id}`] = elements[i].value;
-        }
-      }
-      if (play) {
+      if (this.play) {
         this.aaids.forEach((i) => {
           document.getElementById(`${i}`).textContent = "No File Chosen";
         });
--- src/services/FormService.js
+++ src/services/FormService.js
@@ -1,54 +1,51 @@
-class FormService {
-  files = [];
-  fields = {};
-  aaids = [];
-  fieldBC = "#deeaeb";
-  constructor(element) {
-    this.form = element.form;
-    this.elements = this.form.elements;
-    this.play = true;
-  }
-  chkEmptyElm() {
-    for (let i = 0; i < this.elements.length; i++) {
+export default {
+  chkEmptyElm(element) {
+    const files = [];
+    const fields = {};
+    const aaids = [];
+    let play = true;
+    const fieldBC = "#deeaeb";
+    const form = element.form;
+    const elements = form.elements;
+    for (let i = 0; i < elements.length; i++) {
       //Searching for input files
       if (
-        this.elements[i].nodeName.toLowerCase() == "input" &&
-        this.elements[i].type.toLowerCase() == "file"
+        elements[i].nodeName.toLowerCase() == "input" &&
+        elements[i].type.toLowerCase() == "file"
       ) {
-        const aaid = `${this.elements[i].id}-p`;
+        const aaid = `${elements[i].id}-p`;
         //if there is a selected file
-        if (this.elements[i].files[0] != undefined) {
-          this.files.push(this.elements[i].files[0]);
-          this.aaids.push(aaid);
+        if (elements[i].files[0] != undefined) {
+          files.push(elements[i].files[0]);
+          aaids.push(aaid);
         } else {
           //there is no selected file
-          const labelid = `${this.elements[i].id}-l`;
+          const labelid = `${elements[i].id}-l`;
           const label = document.getElementById(`${labelid}`);
           const aa = document.getElementById(`${aaid}`);
           alert(`Please Select ${label.textContent} File`);
           label.style.color = "red";
           aa.style.color = "red";
-          this.play = false;
+          play = false;
           break;
         }
-      } else if (this.elements[i].nodeName.toLowerCase() != "textarea") {
+      } else if (elements[i].nodeName.toLowerCase() != "textarea") {
         //The field is an input type text or select
-        if (this.elements[i].value != "") {
-          this.fields[`${this.elements[i].id}`] = this.elements[i].value;
-          this.elements[i].style.borderColor = this.fieldBC;
+        if (elements[i].value != "") {
+          fields[`${elements[i].id}`] = elements[i].value;
+          elements[i].style.borderColor = fieldBC;
         } else {
           // this field is empty
-          this.elements[i].style.borderColor = "red";
+          elements[i].style.borderColor = "red";
           alert("Please Fill In the Required Fields in Red");
-          this.play = false;
+          play = false;
           break;
         }
       } else {
         //the field is the textArea
-        this.fields[`${this.elements[i].id}`] = this.elements[i].value;
+        fields[`${elements[i].id}`] = elements[i].value;
       }
     }
-    return [this.files, this.fields, this.play];
-  }
-}
-export { FormService };
+    return [files, fields, aaids, play];
+  },
+};
