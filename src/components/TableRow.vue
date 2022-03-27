a4195a20a2f86ff311eab3fa77ec90b937c840a5
Hector Vurchio <hectorvurchio@gmail.com>
2022-03-27 03:20:23 -0400

    Fixing table 2

--- src/components/TableRow.vue
+++ src/components/TableRow.vue
@@ -1,14 +1,6 @@
 <template>
   <tr v-for="item in items" :key="item.id">
-    <!--<td>{{ item.prefix }}</td>
-    <td>{{ item.machine_name }}</td>
-    <td>{{ item.test_number }}</td>
-    <td>{{ item.start_phase }}</td>
-    <td>{{ item.end_phase }}</td>
-    <td>{{ item.start_stroke }}</td>
-    <td>{{ item.end_stroke }}</td>
-    <td>{{ item.bioethanol }}</td>-->
-    <td v-for="element in aaa(item)" :key="element.id">
+    <td v-for="element in elements(item)" :key="element.id">
       {{ element }}
     </td>
   </tr>
@@ -19,33 +11,8 @@ export default {
     items: { Array, required: true },
     mustBe: { Array, required: true },
   },
-  data() {
-    return {
-      rowValues: null,
-    };
-  },
-  created() {
-    //console.log(this.items);
-    //console.log(this.mustBe);
-    this.rows = this.mustBe;
-    //console.log(this.rowValues);
-  },
-  computed: {
-    elements: {
-      set(val) {
-        this.rowValues = "";
-        //if (this.mustBe.includes(val)) {
-        //  this.rowValues = val;
-        //}
-        console.log(val);
-      },
-      get() {
-        return this.rowValues;
-      },
-    },
-  },
   methods: {
-    aaa(val) {
+    elements(val) {
       const bbb = [];
       for (const [key, value] of Object.entries(val)) {
         if (this.mustBe.includes(key)) {
