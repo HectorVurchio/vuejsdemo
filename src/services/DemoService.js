df8a297f50c3d6edadf8f434da478b6dc89cceaf
Hector Vurchio <hectorvurchio@gmail.com>
2022-04-25 15:33:06 -0400

    first deployment

--- src/services/DemoService.js
+++ src/services/DemoService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost",
+  baseURL: "http://129.213.41.157",
   withCredentials: false,
   headers: { "Content-Type": "multipart/form-data" },
 });
