b9691bdf9c742a3321fa2962a529520807b768aa
Hector Vurchio <hectorvurchio@gmail.com>
2022-04-28 23:16:43 -0400

    fixing recursion bug

--- src/components/TableRow.vue
+++ src/components/TableRow.vue
@@ -1,9 +1,9 @@
 <template>
-  <tr v-for="item in items" :key="item.id">
-    <td @click="rowClk" v-for="element in elements(item)" :key="element.id">
+  <tr @click="rowClk" v-for="item in items" :key="item.id">
+    <td v-for="element in elements(item)" :key="element.id">
       {{ element }}
     </td>
-    <input :id="item.id" type="hidden" :value="hide" />
+    <input :id="item.id" type="hidden" :value="getHide(item)" />
   </tr>
 </template>
 <script>
@@ -14,22 +14,17 @@ export default {
   },
   data() {
     return {
-      hide: null,
       vals: null,
     };
   },
   methods: {
     elements(val) {
       const arr = [];
-      const obj = {};
       for (const [key, value] of Object.entries(val)) {
         if (this.mustBe.includes(key)) {
           arr.push(value);
-        } else {
-          obj[`${key}`] = value;
         }
       }
-      this.hide = JSON.stringify(obj);
       return arr;
     },
     rowClk(event) {
@@ -38,6 +33,15 @@ export default {
       this.vals = parent.lastChild.value;
       this.$store.commit("change_uploadval", parent.lastChild.value);
       this.$router.push({ name: "ExelFile", params: { file: "files" } });
+    },
+    getHide(val) {
+      const obj = {};
+      for (const [key, value] of Object.entries(val)) {
+        if (!this.mustBe.includes(key)) {
+          obj[key] = value;
+        }
+      }
+      return JSON.stringify(obj);
     },
   },
 };
